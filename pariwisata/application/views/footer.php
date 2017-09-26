0144027d5096c7bc1308d912e00992c1317e27fa
wahyudistira <wahyudistira1@yahoo.com>
2017-09-26 11:39:42 +0700

    removed folder pariwisata

--- pariwisata/application/views/footer.php
+++
@@ -1,9 +0,0 @@
-
-<div class="footer">
-    
-    <p style="margin-top: 10px; text-align: center; padding-top: 10px">Copyright © 2011 Ministry of Culture and Tourism, Republic of Indonesia. All Rights Reserved</p>
-    
-</div>
-</div> <!--end wrap-->
-</body>
-</html>
